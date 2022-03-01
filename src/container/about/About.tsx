b5f2cd6498b17ae87ebbcfce2a4472ad9d4e4c11
gordun209 <ali-han80@hotmail.com>
2022-03-01 22:06:51 +0300

    fixed some issues about .env file and sanity implementation

--- src/container/about/About.tsx
+++ src/container/about/About.tsx
@@ -1,7 +1,55 @@
 import './About.scss'
+import { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+import { images } from '../../constants'
+import { urlFor, client } from '../../client'
 
 const About = () => {
-  return <div>About</div>
+  const abouts = [
+    {
+      title: 'About Me',
+      description:
+        'I am a full-stack developer with a passion for building web applications. I have a background in business and marketing, and I am currently working as a full-stack developer at a startup in the San Francisco area.',
+      image: ''
+    },
+    {
+      title: 'About Me',
+      description:
+        'I am a full-stack developer with a passion for building web applications. I have a background in business and marketing, and I am currently working as a full-stack developer at a startup in the San Francisco area.',
+      image: ''
+    }
+  ]
+
+  return (
+    <>
+      <h2 className='head-text'>
+        I know that
+        <span> Css </span>
+        <br />
+        is
+        <span> Horse SHit </span>
+      </h2>
+      <div className='app_profiles'>
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+            className='app__profile-item'
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className='bold-text' style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
+          </motion.div>
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default About
