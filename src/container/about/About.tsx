e07947201310a68bea767d044eeb55997bdc4c39
gordun209 <ali-han80@hotmail.com>
2022-03-02 13:01:34 +0300

    added about page info with sanity

--- src/container/about/About.tsx
+++ src/container/about/About.tsx
@@ -1,25 +1,26 @@
 import './About.scss'
-import { useState, useEffect } from 'react'
+
+import { SanityImageSource } from '@sanity/image-url/lib/types/types'
 import { motion } from 'framer-motion'
-import { images } from '../../constants'
-import { urlFor, client } from '../../client'
+import { useEffect, useState } from 'react'
+
+import { client, urlFor } from '../../client'
 
 const About = () => {
-  const abouts = [
-    {
-      title: 'About Me',
-      description:
-        'I am a full-stack developer with a passion for building web applications. I have a background in business and marketing, and I am currently working as a full-stack developer at a startup in the San Francisco area.',
-      image: ''
-    },
-    {
-      title: 'About Me',
-      description:
-        'I am a full-stack developer with a passion for building web applications. I have a background in business and marketing, and I am currently working as a full-stack developer at a startup in the San Francisco area.',
-      image: ''
-    }
-  ]
+  interface IAbout {
+    imgUrl: SanityImageSource
+    title: string
+    description: string
+  }
 
+  const [abouts, setAbouts] = useState<IAbout[]>([])
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+    client.fetch(query).then(res => {
+      setAbouts(res)
+    })
+  }, [])
+  console.log(abouts)
   return (
     <>
       <h2 className='head-text'>
@@ -29,20 +30,20 @@ const About = () => {
         is
         <span> Horse SHit </span>
       </h2>
-      <div className='app_profiles'>
+      <div className='app__profiles'>
         {abouts.map((about, index) => (
           <motion.div
+            key={about.title + index}
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: 'tween' }}
             className='app__profile-item'
-            key={about.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl) as any} alt={about.title} />
             <h2 className='bold-text' style={{ marginTop: 20 }}>
               {about.title}
             </h2>
-            <p className='bold-text' style={{ marginTop: 10 }}>
+            <p className='p-text' style={{ marginTop: 10 }}>
               {about.description}
             </p>
           </motion.div>
